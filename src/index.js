3e032bb7ab1397d1dfea8321cf3b2f4d107c6606
Hramchenlo Alexey <qwefix@gmail.com>
2020-10-05 21:28:28 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrOfStr=str.split('');
+  let bracketsScript=[];
+  for(let symbol of arrOfStr){
+    let pushed=false;
+    if(symbol==bracketsScript[bracketsScript.length-1]){
+      bracketsScript.pop();
+      continue
+    }else{
+      for(let i=0 ;i<bracketsConfig.length;i++){
+        if(symbol==bracketsConfig[i][0]){
+          bracketsScript.push(bracketsConfig[i][1]);
+          pushed=true;
+        }
+      }
+    }
+    if(pushed) continue;
+    return false
+    
+  }
+  if (bracketsScript==0) return true
+  return false
 }
